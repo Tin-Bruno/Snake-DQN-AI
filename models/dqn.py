f3e738696272803c954d0e8211970d89c913f1ba
Bruno David M. De A. Rodrigues <53412733+Tin-Bruno@users.noreply.github.com>
2026-05-26 16:42:55 -0300

    Implement DQN class with neural network structure

--- models/dqn.py
+++ models/dqn.py
@@ -1,1 +1,18 @@
+import torch
+from torch import nn
 
+
+class DQN(nn.Module):
+    def __init__(self, input_size=11, output_size=3):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_size),
+        )
+
+    def forward(self, x):
+        return self.net(x)
